3a4e5cdd7706318f13557f1b294d5fd5f66e8024
ashish <ashishyadav4978@gmail.com>
2026-09-03 22:37:57 +0530

    removed hyprland border

--- hypr/modules/looknfeel.lua
+++ hypr/modules/looknfeel.lua
@@ -4,62 +4,54 @@
 
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
-	general = {
-		gaps_in = 2,
-		gaps_out = 4,
+  general = {
+    gaps_in = 2,
+    gaps_out = 4,
 
-		border_size = 1,
+    -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+    resize_on_border = true,
 
-		col = {
-			-- active_border = { colors = { "rgba(1d2021ff)", "rgba(ffa89984)" }, angle = 45 },
-			active_border = "rgba(ffa89984)",
-			inactive_border = "rgba(595959aa)",
-		},
+    -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+    allow_tearing = false,
 
-		-- Set to true to enable resizing windows by clicking and dragging on borders and gaps
-		resize_on_border = true,
+    layout = "dwindle",
+  },
 
-		-- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
-		allow_tearing = false,
+  decoration = {
+    rounding = 10,
+    rounding_power = 10,
 
-		layout = "dwindle",
-	},
+    -- Change transparency of focused and unfocused windows
+    active_opacity = 0.95,
+    inactive_opacity = 0.88,
+    -- active_opacity = 1.0,
+    -- inactive_opacity = 1.0,
 
-	decoration = {
-		rounding = 10,
-		rounding_power = 10,
+    shadow = {
+      enabled = false,
+      range = 4,
+      render_power = 3,
+      color = 0xee1a1a1a,
+    },
 
-		-- Change transparency of focused and unfocused windows
-		active_opacity = 0.95,
-		inactive_opacity = 0.88,
-		-- active_opacity = 1.0,
-		-- inactive_opacity = 1.0,
+    blur = {
+      enabled = true,
+      size = 4,
+      passes = 3,
+      vibrancy = 0.15,
+      contrast = 2.5,
+      brightness = 0.8,
+      new_optimizations = true,
+    },
 
-		shadow = {
-			enabled = false,
-			range = 4,
-			render_power = 3,
-			color = 0xee1a1a1a,
-		},
+    glow = {
+      enabled = false,
+    },
+  },
 
-		blur = {
-			enabled = true,
-			size = 4,
-			passes = 3,
-			vibrancy = 0.15,
-			contrast = 2.5,
-			brightness = 0.8,
-			new_optimizations = true,
-		},
-
-		glow = {
-			enabled = false,
-		},
-	},
-
-	animations = {
-		enabled = true,
-	},
+  animations = {
+    enabled = true,
+  },
 })
 
 -- Ref https://wiki.hypr.land/Configuring/Basics/Workspace-Rules/
@@ -82,21 +74,21 @@ hl.config({
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Dwindle-Layout/ for more
 hl.config({
-	dwindle = {
-		preserve_split = true, -- You probably want this
-	},
+  dwindle = {
+    preserve_split = true, -- You probably want this
+  },
 })
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Master-Layout/ for more
 hl.config({
-	master = {
-		new_status = "master",
-	},
+  master = {
+    new_status = "master",
+  },
 })
 
 -- See https://wiki.hypr.land/Configuring/Layouts/Scrolling-Layout/ for more
 hl.config({
-	scrolling = {
-		fullscreen_on_one_column = true,
-	},
+  scrolling = {
+    fullscreen_on_one_column = true,
+  },
 })
