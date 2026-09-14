0eaaa7c651e38bae7184f392bcdb1149c009dccd
Ashish <ashishyadav4978@gmail.com>
2026-09-14 22:41:40 +0530

    Replace polkit-gnome agent with hyprpolkitagent

--- hypr/modules/autostart.lua
+++ hypr/modules/autostart.lua
@@ -5,5 +5,5 @@
 hl.on("hyprland.start", function()
 	hl.exec_cmd("awww-daemon")
 	hl.exec_cmd("waybar & swaync & swayosd-server")
-	hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+	hl.exec_cmd("systemctl --user start hyprpolkitagent.service")
 end)
